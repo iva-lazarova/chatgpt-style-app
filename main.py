625ee37aa05566efd43ff19f812fc610b6318d34
iva <ivanela.lazarova@gmail.com>
2024-01-18 16:43:33 +0800

    Add Chat memory with ConversationBufferMemory

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import os
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage
 from langchain.chains import LLMChain
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts import (ChatPromptTemplate, HumanMessagePromptTemplate,
+                               MessagesPlaceholder)
+from langchain.memory import ConversationBufferMemory
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -12,16 +14,26 @@ llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
 prompt = ChatPromptTemplate(
         input_variables=["content"],
         messages=[SystemMessage(content="You are a chatbot talking to a human"),
+                  # Where memory will be stored
+                  MessagesPlaceholder(variable_name="chat history"),
                   HumanMessagePromptTemplate.from_template("{content}")
         ]
 )
 
-chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+memory = ConversationBufferMemory(
+        memory_key="chat history",
+        return_messages=True
+)
+
+chain = LLMChain(llm=llm,
+        prompt=prompt,
+        memory=memory,
+        verbose=True)
 
 while True:
     content = input("Your prompt:")
     if content in ["quit", "exit", "bye"]:
-        print("Goodbye")
+        print("Goodbye!")
         break
 
     response = chain.run({"content": content})
